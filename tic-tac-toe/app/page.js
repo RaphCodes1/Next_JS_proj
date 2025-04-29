097217fc27671c2de1c0a7aead229171ff24d46f
RaphCodes1 <raphaelcreer@gmail.com>
2025-04-29 05:19:29 +0400

    added metadata in page.js

--- tic-tac-toe/app/page.js
+++ tic-tac-toe/app/page.js
@@ -1,9 +1,13 @@
 import GameButtons from "@/components/game-buttons/game-buttons";
-
+import Head from 'next/head';
 export default function Home() {
   
   return (
     <>
+      <Head>
+        <title>tic tac toe game</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <GameButtons />
     </>
 
